e511784d836e218160863811c06d709a94ccd149
hasan-mia <hasanrafi69@gmail.com>
2022-10-01 17:43:47 +0600

    added env

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Port
 const port = process.env.PORT || 5001
 
-const uri = `mongodb+srv://onnorokompathshala:hasanrafi69@cluster0.tsmuzfz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsmuzfz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 //==============================//
